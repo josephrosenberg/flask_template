fec6a0be7adc4543173a257b0d345fc2022db578
Joseph Rosenberg <jofus101@gmail.com>
2015-06-03 12:56:56 -0400

    Update files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os # to access ports
 from flask import Flask
 from flask import render_template
 from flask import request
-from import makeUpper
+from other_file import otherFileFunction
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ def hello_world():
 
 
 @app.route('/', methods=['POST'])
-def pig_latin():
+def handle_input():
 	form1 = makeUpper(request.form['inp1'])
 	form2 = request.form['inp2']
 	return render_template('results.html', temp1=form1, temp2=form2)
